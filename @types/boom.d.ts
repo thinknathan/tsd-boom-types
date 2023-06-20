b80f17267cd2e71b9050fd39d40d874ff8f387db
Nathan Bolton <nathan@nathanbolton.ca>
2023-06-19 17:23:03 -0700

    Initial push

---
+++ @types/boom.d.ts
@@ -0,0 +1,688 @@
+/// <library version="1.0.1" src="https://github.com/britzl/boom/archive/refs/tags/1.0.0.zip" />
+/// <reference types="@typescript-to-lua/language-extensions" />
+/// <reference types="@ts-defold/types" />
+
+/** @noSelfInFile */
+
+interface Vec2 {
+	x: number;
+	y: number;
+	z: 0;
+	/**
+	 * Vec2(0, 1)
+	 */
+	UP: Vec2;
+	/**
+	 * Vec2(0, -1)
+	 */
+	DOWN: Vec2;
+	/**
+	 * Vec2(-1, 0)
+	 */
+	LEFT: Vec2;
+	/**
+	 * Vec2(1, 0)
+	 */
+	RIGHT: Vec2;
+}
+
+interface Anchor {}
+
+interface AreaComp {
+	/**
+	 * Get all collisions currently happening for this component.
+	 * @returns List of collisions
+	 */
+	get_collisions(): Collision[];
+	/**
+	 * Check collision between this component and another object.
+	 * @param other_object The game object to check collisions with.
+	 * @returns boolean (Return true if colliding with the other object), Collision data
+	 */
+	check_collision(
+		other_object: GameObject
+	): LuaMultiReturn<[boolean, Collision]>;
+	/**
+	 * Register event listener when this component is colliding.
+	 * @param tag Optional tag which colliding object must have, nil for all collisions
+	 * @param cb Function to call when collision is detected
+	 */
+	on_collide(tag: string | undefined, cb: () => void): void;
+	/**
+	 * Register event listener when this component is clicked.
+	 * @param cb Function to call when clicked
+	 */
+	on_click(cb: () => void): void;
+	/**
+	 * Check if a point is within the area of this component.
+	 * @param point The point to check
+	 * @returns boolean Will return true if point is within area
+	 */
+	has_point(point: undefined): boolean;
+}
+
+interface Collision {}
+
+interface BodyComp {
+	/**
+	 * Add upward force
+	 * @param force The upward force to apply
+	 */
+	jump(force: number): void;
+}
+
+interface ColorComp {}
+
+interface DoubleJumpComp {
+	/**
+	 * Performs double jump (the initial jump only happens if player is grounded).
+	 * @param force The upward force to apply
+	 */
+	double_jump(force: number): void;
+}
+
+interface FadeInComp {}
+
+interface Fixed {}
+
+interface HealthComp {
+	/**
+	 * Register an event that runs when heal() is called.
+	 * @param cb Function to call
+	 */
+	on_heal(cb: () => void): void;
+	/**
+	 * Register an event that runs when hurt() is called.
+	 * @param cb Function to call
+	 */
+	on_hurt(cb: () => void): void;
+	/**
+	 * Register an event that runs when health is 0 or less.
+	 * @param cb Function to call
+	 */
+	on_death(cb: () => void): void;
+	/**
+	 * Increase hp. Will trigger on_heal.
+	 * @param n Amount to increase
+	 */
+	heal(n: number): void;
+	/**
+	 * Decrease hp. Will trigger on_hurt
+	 * @param n Amount to decrease
+	 */
+	hurt(n: number): void;
+}
+
+interface Lifespan {}
+
+interface Move {}
+
+interface Offscreen {
+	/**
+	 * Register a callback that runs when the object goes out of view
+	 * @param cb Function to call when the object goes out of view
+	 */
+	on_exit_screen(cb: () => void): void;
+	/**
+	 * Register a callback that runs when the object enters view
+	 * @param cb Function to call when the object enters view
+	 */
+	on_enter_screen(cb: () => void): void;
+}
+
+interface Opacity {}
+
+interface Pos {
+	/**
+	 * Move a number of pixels per second.
+	 * @param x
+	 * @param y
+	 */
+	move(x: number, y: number): void;
+}
+
+interface Rotate {}
+
+interface Scale {}
+
+interface Sprite {
+	/**
+	 * Play an animation
+	 * @param anim The animation to play
+	 */
+	play(anim: string): void;
+	/**
+	 * Stop the current animation
+	 */
+	stop(): void;
+}
+
+interface Stay {}
+
+interface Text {}
+
+interface Timer {
+	/**
+	 * Run a callback function after n seconds
+	 * @param n Seconds
+	 * @param fn The function to call
+	 */
+	wait(n: number, fn: () => void): void;
+	/**
+	 * Run a callback function every n seconds
+	 * @param n Seconds
+	 * @param fn The function to call
+	 */
+	loop(n: number, fn: () => void): void;
+	/**
+	 * Cancel the timer
+	 */
+	cancel(): void;
+}
+
+interface Z {}
+
+interface GameObject {
+	/**
+	 * Add a game object as a child of this game object.
+	 * @param comps The game object components
+	 * @returns The game object
+	 */
+	add(comps: Component[]): GameObject;
+	/**
+	 * Destroy this game object
+	 */
+	destroy(): void;
+	/**
+	 * Check if there is a certain tag on this game object.
+	 * @param tag The tag to check
+	 * @returns Returns true if the tag exists on the game object
+	 */
+	is(tag: string): boolean;
+	/**
+	 * Add a component to this game object.
+	 * @param comp The component to use
+	 */
+	use(comp: object): void;
+	/**
+	 * Remove a component from this game object.
+	 * @param tag The component tag to remove
+	 */
+	unuse(tag: string): void;
+	/**
+	 * Get state for a specific component on this game object.
+	 * @param tag The component to get state for
+	 * @returns The component state
+	 */
+	c(tag: string): object;
+}
+
+interface Tween {
+	/**
+	 * Register an event when finished
+	 * @param fn The function to call when the tween has finished
+	 */
+	on_end(fn: () => void): void;
+	/**
+	 * Finish tween now.
+	 */
+	finish(): void;
+	/**
+	 * Cancel tween.
+	 */
+	cancel(): void;
+}
+
+type Component =
+	| Anchor
+	| AreaComp
+	| BodyComp
+	| ColorComp
+	| DoubleJumpComp
+	| FadeInComp
+	| Fixed
+	| HealthComp
+	| Lifespan
+	| Move
+	| Offscreen
+	| Opacity
+	| Pos
+	| Rotate
+	| Scale
+	| Sprite
+	| Stay
+	| Text
+	| Timer
+	| Z;
+
+// Functions
+
+/**
+ * Start a boom game. Call this from your own game script
+ * @param game Game loop function
+ */
+declare function boom(game: () => void): void;
+
+/**
+ * Anchor point for render.
+ * @param anchor Anchor (center, topleft, left, topright, right, bottomright, bottom, bottomleft)
+ * @return The anchor component.
+ */
+declare function anchor(
+	anchor:
+		| 'center'
+		| 'topleft'
+		| 'left'
+		| 'topright'
+		| 'right'
+		| 'bottomright'
+		| 'bottom'
+		| 'bottomleft'
+): Anchor;
+
+/**
+ * Create a collider area and enabled collision detection. This will create an area component which is used to describe an area which can collide with other area components.
+ * @param options Component options (width and height)
+ * @returns The area component
+ */
+declare function area(options: { width: number; height: number }): AreaComp;
+
+/**
+ * Physical body that responds to gravity. Requires AreaComp and PosComp components on the game object. This also makes the object solid.
+ * @param options Component options (jump_force, is_static)
+ * @returns The body component
+ */
+declare function body(options: {
+	jump_force: number;
+	is_static: boolean;
+}): BodyComp;
+
+/**
+ * Create a color component
+ * @param
+ * @returns The color component
+ */
+declare type color = [r: number, g: number, b: number, a: number | undefined];
+declare function color(args: ColorComp | color): ColorComp;
+
+/**
+ * Enables double jump. Requires body component
+ * @param options Component options
+ * @returns The double jump component
+ */
+declare function double_jump(options: { num_jumps?: number }): DoubleJumpComp;
+
+/**
+ * Fade object in.
+ * @param time In seconds
+ * @returns The fade in component.
+ */
+declare function fadein(time: number): FadeInComp;
+
+/**
+ * Create a fixed component
+ * @returns The component
+ */
+declare function fixed(): Fixed;
+
+/**
+ * Create a health component
+ * @param hp Initial health
+ * @returns The health component
+ */
+declare function health(hp: number): HealthComp;
+
+/**
+ * Create a Lifespan component.
+ * @param time In seconds
+ * @param options Fade out duration (default 0 which is no fade out).
+ */
+declare function lifespan(time: number, options: { fade?: number }): Lifespan;
+
+/**
+ * Create a move component.
+ * @param direction Direction of movement.
+ * @param speed Speed of movement in pixels per second.
+ * @returns The created component.
+ */
+declare function move(direction: Vec2, speed: number): Move;
+
+/**
+ * Create an offscreen component.
+ * @param options Destroy when going offscreen
+ * @returns The created component
+ */
+declare function offscreen(options: {
+	distance: number;
+	destroy: boolean;
+}): Offscreen;
+
+/**
+ * Create an opacity component.
+ * @param value The opacity from 0.0 to 1.0
+ * @returns The created component
+ */
+declare function opacity(value: number): Opacity;
+
+/**
+ * Create a position component.
+ * @param x
+ * @param y
+ * @returns The created component
+ */
+declare function pos(x: number, y: number): Pos;
+
+/**
+ * Apply rotation to object
+ * @param angle
+ * @returns The component
+ */
+declare function rotate(angle: number): Rotate;
+
+/**
+ * Apply a scale to the object
+ * @param x
+ * @param y
+ * @returns The component
+ */
+declare function scale(x: number, y: number): Scale;
+
+/**
+ * Render a sprite.
+ * @param sheet
+ * @param animations
+ */
+declare function sprite(
+	anim: string,
+	options: {
+		atlas?: any;
+		flip_x?: boolean;
+		flip_y?: boolean;
+		width?: number;
+		height?: number;
+	}
+): Sprite;
+
+/**
+ * Do not get destroyed on scene switch.
+ * @returns The created component
+ */
+declare function stay(): Stay;
+
+/**
+ * Render text.
+ * @param content The text to show
+ * @param options Text options (width, font, align)
+ * @returns The created component
+ */
+declare function text(
+	text: string,
+	options: {
+		font?: string;
+		align?: 'left' | 'right' | 'center';
+		width?: number;
+	}
+): Text;
+
+/**
+ * Run certain action after some time.
+ * @param n Number of seconds to wait
+ * @param fn The function to call
+ * @returns The created component
+ */
+declare function timer(n: number, fn: () => void): Timer;
+
+/**
+ * Determines the draw order for objects. Object will be drawn on top if z value is bigger.
+ * @param index Z-value of the object.
+ * @returns The created component
+ */
+declare function z(index: number): Z;
+
+/**
+ * Register an event that runs when two game objects collide.
+ * @param tag1 Tag which the first game object must have
+ * @param tag2 Optional tag which the second game object must have
+ * @param fn Will receive (collision, cancel) as args
+ * @returns Cancel event function
+ */
+declare function on_collide(
+	tag1: string | undefined,
+	tag2: string | undefined,
+	fn: (collision: any, cancel: any) => void
+): any;
+
+/**
+ * Set mouse click listener.
+ * @param tag Optional click on object with tag filter
+ * @param cb Callback when mouse button is clicked
+ * @returns Cancel listener function
+ */
+declare function on_click(tag: string | undefined, cb: () => void): any;
+
+/**
+ * Register callback that runs when a certain key is pressed.
+ * @param key_id The key that must be pressed or nil for any key
+ * @param cb The callback
+ * @returns Cancel callback
+ */
+declare function on_key_press(
+	key_id: string | undefined,
+	cb: () => void
+): () => any;
+
+/**
+ * Register callback that runs when a certain key is released.
+ * @param key_id The key that must be released or nil for any key
+ * @param cb The callback
+ * @returns Cancel callback
+ */
+declare function on_key_release(
+	key_id: string | undefined,
+	cb: () => void
+): () => void;
+
+/**
+ * Check if a certain key is down.
+ * @param key_id The key that must be down, or nil for any key
+ * @returns True if down
+ */
+declare function is_key_down(key_id: string | undefined): boolean;
+
+/**
+ * Get mouse position (screen coordinates).
+ * @returns Mouse position
+ */
+declare function mouse_pos(): Vec2;
+
+/**
+ * Run a function every frame. Register an event that runs every frame, optionally for all game objects with certain tag
+ * @param tag Run event for all objects matching tag (optional)
+ * @param fn The event function to call. Will receive object and cancel function.
+ */
+declare function on_update(
+	tag: string | undefined,
+	fn: (object: any, cancel: (object: any, cancel: any) => void) => void
+): void;
+
+/**
+ * Add a game object with a set of components.
+ * @param comps The components for the game object
+ * @returns The created game object
+ */
+declare function add(comps: Component[]): GameObject;
+
+/**
+ * Destroy a game object and all of its components.
+ * @param object The object to destroy
+ */
+declare function destroy(object: GameObject): void;
+/**
+ * Destroy all objects with a certain tag.
+ * @param tag The tag to destroy or nil to destroy all objects
+ */
+declare function destroy_all(tag: string): void;
+/**
+ * Get game object with specific id.
+ * @param id
+ * @returns The object or nil if it doesn't exist
+ */
+declare function object(id: string): GameObject | undefined;
+/**
+ * Get all game objects.
+ * @returns All game objects
+ */
+declare function objects(): GameObject[];
+/**
+ * Get all game objects with the specified tag.
+ * @param tag The tag to get objects for, nil to get all objects
+ * @returns List of objects
+ */
+declare function get(tag: string | undefined): GameObject[];
+/**
+ * Run callback on every object with a certain tag.
+ * @param tag The tag that must exist on the object
+ * @param cb The callback to run
+ */
+declare function every(tag: string, cb: (object: GameObject) => void): void;
+/**
+ * Get or set camera position.
+ * @param x
+ * @param y
+ * @returns Camera position
+ */
+declare function cam_pos(x?: number | Vec2, y?: number): Vec2;
+/**
+ * Get or set camera rotation.
+ * @param angle The angle to set or nil to get current rotation
+ * @returns The camera rotation in degrees
+ */
+declare function cam_rot(angle?: number): number;
+/**
+ * Get or set the camera zoom.
+ * @param zoom The zoom to set or nil to get the current zoom.
+ * @returns The camera zoom
+ */
+declare function cam_zoom(zoom?: number): number;
+/**
+ * Get gravity
+ * @returns The gravity in pixels per seconds
+ */
+declare function get_gravity(): number;
+/**
+ * Set gravity
+ * @param gravity Gravity in pixels per seconds
+ */
+declare function set_gravity(gravity: number): void;
+/**
+ * Get screen width
+ * @returns Width of screen
+ */
+declare function width(): number;
+/**
+ * Get screen height
+ * @returns Height of screen
+ */
+declare function height(): number;
+/**
+ * Get screen center position
+ * @returns Center of screen
+ */
+declare function center(): Vec2;
+/**
+ * Get the delta time
+ * @returns Delta time
+ */
+declare function dt(): number;
+/**
+ * Get time since start
+ * @returns Time since start in seconds
+ */
+declare function time(): number;
+/**
+ * Construct a level based on symbols.
+ * @param map List of strings presenting horizontal rows of tiles
+ * @param options Level options (tile_width, tile_height, pos, tiles)
+ * @returns Game object with tiles as children
+ */
+declare function add_level(
+	map: string[] | LuaTable<string>,
+	options: {
+		tile_width?: number;
+		tile_height?: number;
+		pos?: Vec2;
+		tiles?: {
+			[key: string]: () => Component[];
+		};
+	}
+): GameObject;
+/**
+ * Get a random number. If called with no arguments the function returns a number between 0 and 1. If called with a single argument 'a' a number between 0 and 'a' is returned. If called with two arguments 'a' and 'b' a number between 'a' and 'b' is returned.
+ * @param a
+ * @param b
+ * @returns Float number
+ */
+declare function rand(a?: number, b?: number): number;
+
+/**
+ * Same as rand() but floored.
+ * @param a
+ * @param b
+ * @returns Integer number
+ */
+declare function randi(a?: number, b?: number): number;
+
+/**
+ * Tween a value from one to another. The transition will happen over a certain duration using a specific easing function.
+ * @param from Start value
+ * @param to End value
+ * @param duration Time in seconds to go from start to end value
+ * @param easing Which easing algorithm to use
+ * @param set_value Function to call when the value has changed
+ * @returns A tween object.
+ */
+declare function tween(
+	from: number | Vec2,
+	to: number | Vec2,
+	duration: number,
+	easing: string,
+	set_value: (value: number | Vec2) => void
+): Tween;
+
+/**
+ * Create a Vec2
+ * @param x Horizontal position
+ * @param y Vertical position
+ * @returns The created Vec2
+ */
+declare function vec2(x: number, y: number): Vec2;
+
+/**
+ * Create a scene.
+ * @param id Unique id of the scene
+ * @param fn The scene code
+ */
+declare function scene(id: string, fn: (...args: any[]) => void): void;
+
+/**
+ * Show a scene.
+ * @param id Id of the scene to show
+ * @param args Additional arguments to pass to the scene function
+ */
+declare function show(id: string, ...args: any[]): void;
+
+/**
+ * Run a callback after a certain nummber of seconds.
+ * @param seconds Number of seconds to wait
+ * @param cb Function to call
+ * @returns Call to cancel the timer
+ */
+declare function wait(seconds: number, cb: () => void): () => any;
+
+/**
+ * Run a callback repeatedly with a certain interval
+ * @param seconds Interval between calls
+ * @param cb Function to call
+ * @returns Call to cancel the timer
+ */
+declare function loop(seconds: number, cb: () => void): () => any;
